d724c7a76677b164e088786cf58554460e076aa8
sohil101 <104003079+sohil101@users.noreply.github.com>
2023-07-17 22:01:15 +0530

    Update addresses.js

--- data/addresses.js
+++ data/addresses.js
@@ -1,69 +1,30 @@
-const addresses = [
-  {
-    name: 'John Doe',
-    address: '123 Main St',
-    location: {
-      type: 'Point',
-      coordinates: [-73.9712, 40.7831]
-    }
-  },
-  {
-    name: 'Jane Smith',
-    address: '456 Elm St',
-    location: {
-      type: 'Point',
-      coordinates: [-73.9957, 40.7294]
-    }
-  },
-  {
-    name: 'Bob Johnson',
-    address: '789 Maple Ave',
-    location: {
-      type: 'Point',
-      coordinates: [-73.9586, 40.8003]
-    }
-  },
-  {
-    name: 'Eiffel Tower',
-    address: 'Champ de Mars, 5 Avenue Anatole France, 75007 Paris, France',
-    location: {
-      type: 'Point',
-      coordinates: [2.2945, 48.8584]
-    }
-  },
-  {
-    name: 'Statue of Liberty',
-    address: 'New York, NY 10004, United States',
-    location: {
-      type: 'Point',
-      coordinates: [-74.0445, 40.6892]
-    }
-  },
-  {
-    name: 'Big Ben',
-    address: 'Westminster, London SW1A 0AA, United Kingdom',
-    location: {
-      type: 'Point',
-      coordinates: [-0.126236, 51.5007]
-    }
-  },
-  {
-    name: 'Golden Gate Bridge',
-    address: 'Golden Gate Bridge, San Francisco, CA, United States',
-    location: {
-      type: 'Point',
-      coordinates: [-122.4786, 37.8199]
-    }
-  },
-  {
-    name: 'Taj Mahal',
-    address: 'Dharmapuri, Forest Colony, Tajganj, Agra, Uttar Pradesh 282001, India',
-    location: {
-      type: 'Point',
-      coordinates: [78.0422, 27.1750]
-    }
-  }
-]
+const mongoose = require('mongoose');
 
+const addressSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  location: {
+    type: {
+      type: String,
+      enum: ['Point'],
+      required: true,
+    },
+    coordinates: {
+      type: [Number],
+      required: true,
+    },
+  },
+});
 
-module.exports = addresses;
+// Add a 2dsphere index for the location field
+addressSchema.index({ location: '2dsphere' });
+
+const Address = mongoose.model('Address', addressSchema);
+
+module.exports = Address;
